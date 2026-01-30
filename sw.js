6df98c7eac865bbc4c0957ee113bde1ae156f790
nzp5002 <antonio.danilo3652024@gmail.com>
2026-01-30 09:39:58 -0300

    Update sw.js
    Signed-off-by: nzp5002 <antonio.danilo3652024@gmail.com>

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10542506
+    "zoneId": 10542512
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw
